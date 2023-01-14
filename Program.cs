eabc1ff6a85e46d94ac8088c7528c45ab49467fa
Artefius <kostikixum@gmail.com>
2023-01-14 15:07:44 +0700

    исправлена ошибка индекса

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
-﻿void PrintArray(string[] array)
+﻿void PrintArrayString(string[] array)
+{
+    for (int a = 0; a < array.GetLength(0); a++)
+    {
+
+
+        Console.Write(array[a] + " ");
+    }
+    Console.WriteLine();
+}//метод вывода масива строк
+void PrintArray(int[] array)
 {
     for (int a = 0; a < array.GetLength(0); a++)
     {
@@ -8,22 +18,32 @@
     }
     Console.WriteLine();
 }//метод вывода масива
-string[] array = { "О", "да", "это",
-     "работает", "не", "гуд", };
-Console.WriteLine("Введите индекс");
-int index = Convert.ToInt32(Console.ReadLine());
-string[] newArray = new string[array.Length - 1];//образуем новый масив меньшего размера
-for (int i = 0; i < array.Length; i++)
+string[] array = {"ура","это","нееее",
+     "не работает","run","раб","ота","ет"};
+int m = 3;//максимальный размер строки
+int z = 0;//количество слов котрое больше m
+int y = 0;//index нового масива
+int[] index = new int[array.Length];
+for (int i = 0; i < array.Length - 1; i++)
 {
-    if (i < index)
+    if (array[i].Length > m)
     {
-        newArray[i] = array[i];
+        index[i] = 1; z++;
     }
-    else if (i > index)
+    else
     {
-        newArray[i - 1] = array[i];
+        index[i] = 0;
     }
-}
-PrintArray(newArray);
+}// создаем масив где 1 отмечаны индексы слов больше 3
+string[] newArray = new string[array.Length - z];//образуем новый масив 
+for (int i = 0; i < index.Length; i++)
+{
+    if (index[i] == 0)
+    {
+        newArray[y] = array[i];
+        y++;
+    }
+}//заполняем новый масив словами котрые меньше или равны m;
+PrintArrayString(newArray);
 
 
